85200f0b99870bd2f37ca57603d14faaecd6f94f
tone4ka <77918429+tone4ka0dotcom@users.noreply.github.com>
2025-05-18 13:41:26 +0300

    Update j.py

--- j.py
+++ j.py
@@ -1,3 +1,4 @@
+import sys
 import pygame #импортируем код в pygame
 from random import *
 
@@ -14,7 +15,7 @@ class Sprites():#создание класса
     def draw_image(self):#метод отрисовки картинки
         screen.blit(self.img, (self.rect.x, self.rect.y))
 
-    def move_food(self):
+    def move_money(self): #движение монет
         self.rect.y += 5
 
     def move_plate(self):
@@ -24,40 +25,70 @@ class Sprites():#создание класса
         elif keys[pygame.K_RIGHT]:
             self.rect.x += 5
 
-y4=randint(-500,0)
-y2=randint(-500,0)
-y=randint(-500,0)
-monster1= Sprites("монстр.png",250,y4)
-monster2= Sprites("монстр.png",400,y4)
-monster4 = Sprites("монстр.png",200,y4)
-monster2 = Sprites("монстр.png",350, y)
-monster1 = Sprites("монстр.png",500,y2)
 
-monster_list = [monster1, monster2, monster4]
+
+money1 = Sprites("монстр.png", randint(0,900), randint(-500,0))
+money2 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money3 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money4 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money5 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money6 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money7 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money8 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money9 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money10 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money11 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money12 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money13 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money14 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money15 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money16 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money17 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money18 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money19 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money20 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money21 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money22 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money23 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money24 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money25 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money26 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money27 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money28 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money29 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+money30 = Sprites("монстр.png",randint(0,900), randint(-500,0))
+
+money_list = [money1, money2, money3, money4, money5, money6, money7, money8, money9, money10, money11, money12, money13, money14, money15, money16, money17, money18, money19, money20, money21, money22, money23, money24, money25, money26, money27, money28, money29, money30,]
 fon = Sprites('комнота.jpg', 0, 0)# создание фона
 main_sprite = Sprites('мамин кашелёк.png', 450, 390)# создание фона
 pygame.init()#обезательная программа
 window_size=(930,495)#размеры окна
 screen=pygame.display.set_mode(window_size)#сделать экран с размерами
 clock = pygame.time.Clock() #фпс
+font = pygame.font.SysFont("Arial",30) #создание шрифта
+count = 0
 
 
 while True:#игровой цикл
     fon.draw_image()#приминение метода отрисовки картинки к объкеу klassa Food (фон)
-    main_sprite.draw_image() #приминение метода отрисовки картинки к объкеу klassa Food (тарелка)
+    text = font.render("счёт "+str(count), True, (255, 255, 255))  # создание текста
+    screen.blit(text, (50, 50))  # отрисовка текста
+    main_sprite.draw_image() #приминение метода отрисовки картинки к объкеу klassa
     main_sprite.move_plate()# применение метода к объкту
     clock.tick(40)#40фпс
-    for i in monster_list:
+    for i in money_list:
         i.draw_image()
-        i.move_food()
+        i.move_money()
         if i.rect.y>700:
             i.rect.y = 0
 
-        if main_sprite.rect.colliderect(i.rect):
-            monster_list.remove(i)
-        if monster_list == []:
-            pygame.QUIT()
+        if main_sprite.rect.colliderect(i.rect): #если кошелёк коснулся монеты
+            money_list.remove(i) #удалить монету из списка монет
+            count += 1 #увеличеть переменную count на 1
+
+        if money_list == []: #если список монет пустой
+            sys.exit() #выход из игры
     for event in pygame.event.get():#события
         if event.type == pygame.QUIT:#если нажали крест
-            pygame.QUIT()# выход из игры
+            sys.exit() # выход из игры
     pygame.display.update()  # обновление содержимого экрана
